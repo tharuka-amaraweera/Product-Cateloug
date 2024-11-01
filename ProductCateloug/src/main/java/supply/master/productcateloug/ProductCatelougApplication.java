8fc49ee71221d5a364997f88e30d1068d4de8fde
Tharuka Amaraweera <tharukaamaraweera@Tharukas-MacBook-Air.local>
2024-11-01 11:42:14 +0100

    added Product cateloug

---
+++ ProductCateloug/src/main/java/supply/master/productcateloug/ProductCatelougApplication.java
@@ -0,0 +1,13 @@
+package supply.master.productcateloug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatelougApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCatelougApplication.class, args);
+    }
+
+}
